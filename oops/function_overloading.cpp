811acb2e6e0aacea42230bca0ab887de7a6433f6
abhijit-official <abhijitchavan1717@gmail.com>
2021-03-21 23:38:35 +0530

    added new fun overloding program of cpp

---
+++ oops/function_overloading.cpp
@@ -0,0 +1,59 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+void add(int x, int y ,int z){
+    cout<<x+y+z;
+}
+
+void add(int x, int y){
+    cout<<x+y;
+}
+
+int main()
+{
+    add(2,2,3);
+    add(2,3);
+    return 0;
+}
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// void add(int x, int y){
+
+//     cout<<x+y;
+// }
+// void add(int x, int y, int z){
+
+//     cout<<x+y+z;
+// }
+// int main(){
+
+// add(2,3,4);
+// add(4,4);
+// return 0;
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
